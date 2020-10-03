430729659680cb0e0fde112097b443ab6a3a6322
YaroslavSulyma <45691318+YaroslavSulyma@users.noreply.github.com>
2020-10-03 14:15:11 +0300

    *added transitions between pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
